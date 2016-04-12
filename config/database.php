6c0cc47bb8c741aa2528669d6d36ba56a9551344
Lean Karlo Corpuz <lean@teknolohiya.ph>
2016-04-12 15:16:24 +0800

    mysql config change

--- config/database.php
+++ config/database.php
@@ -49,9 +49,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', '127.0.0.1'),
-            'database'  => env('DB_DATABASE', 'projectcanvas'),
+            'database'  => env('DB_DATABASE', 'teknolohiyadb'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'mysql'),
+            'password'  => env('DB_PASSWORD', 'd46a3c9d4655fa5e'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
